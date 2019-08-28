33d79691bde359dfc0aaad5c7a0d7645b28dc42a
christopherbergstrom <christopherrbergstrom@gmail.com>
2019-08-27 21:39:25 -0700

    styling the main page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 $(document).ready(() => {
-    $("#menuBtn").click(function()
-    {
+    // $("#introText").hide().fadeIn("medium");
+    $("#menuBtn").click(() => {
       // $("#list").css("display", "block");
-      $(".menuItem").toggleClass("w3-hide");
+      // $(".menuItem").toggleClass("w3-hide");
     });
-    $(document).click(function(e)
-    {
+    $(document).click((e) => {
       if(!e.target.matches('#menuBtn'))
       {
-        $(".menuItem").addClass("w3-left");
+        // $(".menuItem").removeClass("w3-right");
+        // $(".menuItem").addClass("w3-left");
       }
+    });
+    $(".contactLink").click(() => {
+      let scrollAmount = $("#contact").offset().top;
+      $('html,body').animate({scrollTop: scrollAmount}, 750);
     });
 });
