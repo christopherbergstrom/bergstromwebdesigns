afe94e593c75741d643af08521d367e304a20e56
christopherbergstrom <christopherrbergstrom@gmail.com>
2020-07-28 00:18:30 -0700

    uikit migration complete

--- js/validate.js
+++ js/validate.js
@@ -1,47 +1,44 @@
-$(document).ready(function()
-{
-    $("#submitBtn").click(function(e)
-    {
+$(document).ready(function () {
+    $("#submitBtn").click(function (e) {
         $("#nameError").html("");
         $("#emailError").html("");
         $("#messageError").html("");
+        $("#name").removeClass("uk-form-danger");
+        $("#email").removeClass("uk-form-danger");
+        $("#message").removeClass("uk-form-danger");
+
         let val = validate();
-        if(val[0] || val[1] || val[2])
-        {
+        if (val[0] || val[1] || val[2]) {
             e.preventDefault();
-            if(val[0])
-            {
+            if (val[0]) {
                 $("#nameError").html("Please enter a valid name");
+                $("#name").addClass("uk-form-danger");
             }
-            if(val[1])
-            {
+            if (val[1]) {
                 $("#emailError").html("Please enter a valid email");
+                $("#email").addClass("uk-form-danger");
             }
-            if(val[2])
-            {
+            if (val[2]) {
                 $("#messageError").html("Please enter a message");
+                $("#message").addClass("uk-form-danger");
             }
-        } 
+        }
     });
 });
-function validate()
-{
+function validate() {
     let name = false;
     let email = false;
     let message = false;
     // if name contains numnbers, name = true
-    if($("#name").val().toLowerCase().indexOf("1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "0") >= 0 || $("#name").val() == "")
-    {
+    if ($("#name").val().toLowerCase().indexOf("1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "0") >= 0 || $("#name").val() == "") {
         name = true;
     }
     // if not a valid email, email = true
-    if($("#email").val().toLowerCase().indexOf("@" || ".com" || ".edu") < 0)
-    {
+    if ($("#email").val().toLowerCase().indexOf("@" || ".com" || ".edu") < 0) {
         email = true;
     }
     // if message is blank, message = true
-    if($("#message").val() == "")
-    {
+    if ($("#message").val() == "") {
         message = true;
     }
     return [name, email, message];
